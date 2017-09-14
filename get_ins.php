b68488c6717f003a661bf2865edcc85067504db4
Mehmet TAMCI <mhmttmc@gmail.com>
2017-09-14 08:35:03 +0300

    Update get_ins.php
    file_get_contents her sunucuda sağlıklı çalışmayabilir. Curl ile scriptinizi destekledim.

--- get_ins.php
+++ get_ins.php
@@ -1,8 +1,18 @@
 <meta charset="UTF-8">
 <?php
 
+function tmc_file_get_contents_curl($url){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); //Set curl to return the data instead of printing it to the browser.
+		curl_setopt($ch, CURLOPT_URL, $url);
+		$data = curl_exec($ch);
+		curl_close($ch);
+		return $data;
+	}
+
 $url = 'https://www.instagram.com/erkankavas/media/';
-$content = file_get_contents($url);
+$content = tmc_file_get_contents_curl($url);
 
 
 $json = json_decode($content, true);
